98eef310c0c789cb853083332de87c8f4f3757de
Dídac García <didaquis@gmail.com>
2020-06-20 10:49:25 +0200

    Navbar has been refactored

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,16 +1,19 @@
 <template>
-	<nav class="navbar docs-navbar is-spaced has-shadow is-primary is-transparent">
-		<div class="container">
-			<div class="navbar-brand">
-				<router-link class="navbar-item" to="/">Home</router-link>
-			</div>
-			<div class="navbar-menu">
-				<div class="navbar-end">
-					<router-link class="navbar-item" to="/">Home</router-link>
-					<router-link class="navbar-item" to="/films">Films</router-link>
-					<router-link class="navbar-item" to="/about">About</router-link>
-				</div>
-			</div>
-		</div>
-	</nav>
+	<b-navbar class="is-spaced has-shadow is-primary is-transparent">
+		<template slot="brand">
+			<b-navbar-item tag="router-link" :to="{ path: '/' }">Home</b-navbar-item>
+
+		</template>
+		<template slot="start">
+			<b-navbar-item tag="a" target="_blank" rel="noopener" href="https://github.com/didaquis/ghibli-vue">
+				Github
+			</b-navbar-item>
+		</template>
+
+		<template slot="end">
+			<b-navbar-item tag="router-link" :to="{ path: '/' }">Home</b-navbar-item>
+			<b-navbar-item tag="router-link" :to="{ path: '/films' }">Films</b-navbar-item>
+			<b-navbar-item tag="router-link" :to="{ path: '/about' }">About</b-navbar-item>
+		</template>
+	</b-navbar>
 </template>

7d32029a162d97687949c030f462b16fca437e63
Dídac García <didaquis@gmail.com>
2020-06-20 11:26:03 +0200

    Added link to official website

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,9 @@
 <template>
 	<div class="about">
 		<h1>This is an about page</h1>
+		<b-navbar-item tag="a" target="_blank" rel="noopener" href="http://www.ghibli.jp/">
+				ghibli.jp
+			</b-navbar-item>
 	</div>
 </template>
 

0d25bf90e999ed33b2599de9efed16eecd2cb1c2
Dídac García <didaquis@gmail.com>
2020-06-21 23:52:54 +0200

    Added 404 page

---
+++ src/components/NotFound.vue
@@ -0,0 +1,9 @@
+<template>
+	<section class="section">
+
+		<section class="container has-text-centered">
+			<h2 class="subtitle">UPS! Error: 404. Page not found <span role="img" aria-label="sad face">☹️</span></h2>
+		</section>
+
+	</section>
+</template>

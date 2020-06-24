15485e0dfccf5cd4b224de042a41c3c3c980efe1
Dídac García <didaquis@gmail.com>
2020-06-24 02:42:20 +0200

    Added Card

---
+++ src/components/FilmCard.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="card has-background-info has-text-white" data-cy="film-card">
+		<div class="card-content">
+			<p class="title has-text-white has-text-centered" data-cy="film-card-title">
+				{{ film.title }}
+			</p>
+			<p class="has-text-white" data-cy="film-card-description">
+				{{ film.description }}
+			</p>
+		</div>
+		<footer class="card-footer" data-cy="film-card-footer">
+			<p class="card-footer-item">
+				<span class="has-text-weight-bold custom-mr-2">Year:</span> {{ film.release_date }}
+				<span class="has-text-weight-bold custom-ml-5 custom-mr-2">Director:</span> {{ film.director }}
+			</p>
+		</footer>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		film: {
+			type: Object,
+			required: true
+		}
+	}
+}
+</script>
--- tests/e2e/specs/FilmsPage.js
+++ tests/e2e/specs/FilmsPage.js
@@ -9,12 +9,38 @@ describe('Films page (stub response from API)', () => {
 		cy.visit('/films')
 	})
 
-	it('Should contain the title of the films', () => {
-		const expectedFilmTitles = ['Castle in the Sky', 'Grave of the Fireflies']
+	describe('For every film', () => {
+		it('Should contain the title of the films', () => {
+			const expectedFilmTitles = ['Castle in the Sky', 'Grave of the Fireflies']
 
-		cy.get('[data-cy=main-content]').find('li').each(($el) => {
-			const text = $el.text().trim()
-			expect(text).to.be.oneOf(expectedFilmTitles)
+			cy.get('[data-cy=film-card-title]').each(($el) => {
+				const text = $el.text().trim()
+				expect(text).to.be.oneOf(expectedFilmTitles)
+			})
+
+			cy.get('[data-cy=film-card-title]').should('have.length', 2)
+		})
+
+		it('Should contain the description of the films', () => {
+			const regexOfValidDescriptions = new RegExp(/The orphan Sheeta inherited a mysterious crystal|In the latter part of World War II/)
+
+			cy.get('[data-cy=film-card-description]').each(($el) => {
+				const text = $el.text().trim()
+				expect(text).to.match(regexOfValidDescriptions)
+			})
+
+			cy.get('[data-cy=film-card-description]').should('have.length', 2)
+		})
+
+		it('Should contain the year and director', () => {
+			const regexOfValidResults = new RegExp(/Year: 1986 Director: Hayao Miyazaki|Year: 1988 Director: Isao Takahata/)
+
+			cy.get('[data-cy=film-card-footer]').each(($el) => {
+				const text = $el.text().trim()
+				expect(text).to.match(regexOfValidResults)
+			})
+
+			cy.get('[data-cy=film-card-footer]').should('have.length', 2)
 		})
 	})
 })

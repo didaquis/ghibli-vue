13eb57e36ef69cc4bf3bebdaec0e76ba4cdee2be
Dídac García <didaquis@gmail.com>
2020-11-29 11:34:41 +0100

    Fix navbar visualization on big screens

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,25 +1,27 @@
 <template>
-	<div data-cy="navbar">
-		<b-navbar class="is-spaced has-shadow is-primary is-transparent">
-			<template slot="brand">
-				<b-navbar-item tag="router-link" :to="{ path: '/' }" data-cy="brand-link">
-					<font-awesome-icon :icon="['fas', 'home']" size="2x" />
-				</b-navbar-item>
-			</template>
+	<div class="has-background-primary">
+		<div data-cy="navbar" class="container">
+			<b-navbar class="is-spaced has-shadow is-primary is-transparent">
+				<template slot="brand">
+					<b-navbar-item tag="router-link" :to="{ path: '/' }" data-cy="brand-link">
+						<font-awesome-icon :icon="['fas', 'home']" size="2x" />
+					</b-navbar-item>
+				</template>
 
-			<template slot="end">
-				<b-navbar-item tag="router-link" :to="{ path: '/home' }">Home</b-navbar-item>
-				<b-navbar-item tag="router-link" :to="{ path: '/films' }">Films</b-navbar-item>
-				<b-navbar-item tag="router-link" :to="{ path: '/timeline' }">Timeline</b-navbar-item>
-				<b-navbar-item tag="router-link" :to="{ path: '/about' }">About</b-navbar-item>
-			</template>
+				<template slot="end">
+					<b-navbar-item tag="router-link" :to="{ path: '/home' }">Home</b-navbar-item>
+					<b-navbar-item tag="router-link" :to="{ path: '/films' }">Films</b-navbar-item>
+					<b-navbar-item tag="router-link" :to="{ path: '/timeline' }">Timeline</b-navbar-item>
+					<b-navbar-item tag="router-link" :to="{ path: '/about' }">About</b-navbar-item>
+				</template>
 
-			<template slot="end">
-				<b-navbar-item tag="a" target="_blank" rel="noopener" href="https://github.com/didaquis/ghibli-vue">
-					<font-awesome-icon :icon="['fab', 'github']" size="lg" :style="{ color: 'black' }" />
-				</b-navbar-item>
-			</template>
-		</b-navbar>
+				<template slot="end">
+					<b-navbar-item tag="a" target="_blank" rel="noopener" href="https://github.com/didaquis/ghibli-vue">
+						<font-awesome-icon :icon="['fab', 'github']" size="lg" :style="{ color: 'black' }" />
+					</b-navbar-item>
+				</template>
+			</b-navbar>
+		</div>
 	</div>
 </template>
 
